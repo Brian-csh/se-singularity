4cf0bb92d69697acf4afe6dac409e53d32efa236
蔡诗怀 <2021080068@secoder.net>
2023-03-30 10:56:37 +0800

    fix: revise database class path (#9)

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -1,9 +1,10 @@
 <?php
+use api\config\Database;
 class DatabaseTest extends \PHPUnit\Framework\TestCase
 {
     public function testConnect()
     {
-        $database = new Database();
+        $database = new Database;
         $this->assertEquals(null, $database->connect());
     }
 }
