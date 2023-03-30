4efd5143909857e3b846ab147b01711f4c728160
蔡诗怀 <2021080068@secoder.net>
2023-03-30 12:59:42 +0800

    fix: edit database test script (#9)

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -1,11 +1,10 @@
 <?php
 require __DIR__ . "/../api/config/Database.php";
-use api\config\Database;
 class DatabaseTest extends \PHPUnit\Framework\TestCase
 {
     public function testConnect()
     {
-        $database = new Database;
+        $database = new Database();
         $this->assertEquals(null, $database->connect());
     }
 }
