5609a7587d58fcdabbae61e0d3daae815c96434c
孙迪往 <2021080087@secoder.net>
2023-04-05 14:25:16 +0000

    Update tests/DatabaseTest.php

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -2,22 +2,6 @@
 require __DIR__ . "/../api/config/Database.php";
 class DatabaseTest extends \PHPUnit\Framework\TestCase
 {
-    // public function assertNotEqualsCustom($expected, $actual, $message = '')
-    // {
-    //     if ($expected === $actual) {
-    //         $defaultMessage = sprintf(
-    //             'Failed asserting that %s matches expected %s.',
-    //             var_export($actual, true),
-    //             var_export($expected, true)
-    //         );
-    //         if ($message !== '') {
-    //             $message = $message . "\n" . $defaultMessage;
-    //         } else {
-    //             $message = $defaultMessage;
-    //         }
-    //         $this->fail($message);
-    //     }
-    // }
     public function testConnect()
     {
         $database = new Database();
