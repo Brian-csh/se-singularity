fde09169c694ec784d1466ef3170c097e829a479
孙迪往 <2021080087@secoder.net>
2023-04-08 20:21:45 +0800

    comments: for feishu_callback.php

--- feishu_callback.php
+++ feishu_callback.php
@@ -3,6 +3,8 @@ require 'includes/db/connect.php';
 
 session_start();
 
+// bind: existing Singularity users can bind their Feishu accs
+// signin: sign in to Singularity w Feishu (not sign up)
 if(isset($_GET['mode'])) {
     $mode = $_GET['mode'];
     if($mode == "bind" || $mode == "signin"){
@@ -10,6 +12,7 @@ if(isset($_GET['mode'])) {
     }
 }
 
+// API creds
 $feishu_app_id = "cli_a4a8e931cd79900e";
 $feishu_app_secret = "7Q1Arabz1qImkNpLOp2D9coj5cXp1ufJ";
 if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['HTTP_HOST'] == 'localhost') {
@@ -26,6 +29,7 @@ $url = "https://passport.feishu.cn/suite/passport/oauth/token";
 $myvars = 'grant_type=authorization_code'.'&client_id=' . $feishu_app_id.'&client_secret=' . $feishu_app_secret . '&code=' . $params['code'] . '&redirect_uri=' . $singularity_redirect;
 $info_url = "https://passport.feishu.cn/suite/passport/oauth/userinfo";
 
+// request access token
 $ch = curl_init( $url );
 curl_setopt( $ch, CURLOPT_POST, 1);
 curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
@@ -37,6 +41,7 @@ $response = curl_exec( $ch );
 
 $arr = json_decode($response, false);
 
+// if succesful in getting the access token, request for user information
 if(isset($arr->access_token)){
     $header = array();
     $header[] = 'Content-length: 0';
@@ -51,21 +56,23 @@ if(isset($arr->access_token)){
     $user_info_str = curl_exec( $ch );
     $user_info = json_decode($user_info_str, true);
     
+    // feishu id
     $sub_id = $user_info["sub"];
 
     // set feishu user id in the database
     if($mode == "bind") {
+        // Update sessions
         $session_user_id = $_SESSION['admin']['id'];
-        echo $session_user_id;
 
         // Construct the SQL update statement
         $sql = "UPDATE user SET feishu_id = '$sub_id' WHERE id = '$session_user_id'";
 
         // Execute the SQL statement
         mysqli_query($conn, $sql);
-            
+        
+        // Redirect
         header('Location: index.php');
-        die();
+        exit();
     }
 
     else if($mode == "signin") {
@@ -88,11 +95,11 @@ if(isset($arr->access_token)){
 
             $_SESSION['admin'] = $row;
             header('Location: index.php');
-            die();
+            exit();
         } else {
             // If feishu_id does not exist, cannot sign in.
             header('Location: signin.php?signin='.urlencode("403"));
-            exit();
+            die();
         }
         // close the statement
         $stmt->close();
@@ -101,7 +108,8 @@ if(isset($arr->access_token)){
 }
 else{
     $user_info = NULL;
-    echo "no access token";
+    header('Location: signin.php?signin='.urlencode("403"));
+    die();
 }
 
 curl_close($ch);
