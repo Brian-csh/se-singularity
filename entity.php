696a9f476ce2f4fdf67b9a38401866649455f9d4
蔡诗怀 <2021080068@secoder.net>
2023-04-08 22:37:24 +0800

    fix: improve ui (#24)

--- entity.php
+++ entity.php
@@ -13,94 +13,91 @@ include "includes/header.php";
 
 <div id="layoutSidenav_content">
     <main>
-        <header class="page-header page-header-compact page-header-light border-bottom bg-black mb-4">
-            <div class="container-fluid px-4">
-                <div class="page-header-content">
-                    <div class="row align-items-center justify-content-between pt-3">
-                        <div class="col-auto mb-3 d-inline w-100">
-                            <h1 class="page-header-title text-white d-inline">
-                                <div class="page-header-icon text-white"><i data-feather="home"></i></div>
-                                <?=$active?>
+    <header class="page-header pt-10 page-header-dark bg-gradient-primary-to-secondary pb-5">
+            <div class="container-xl px-4">
+                <div class="page-header-content pt-4">
+                    <div class="row align-items-center justify-content-between">
+                        <div class="col-auto mt-4">
+                            <h1 class="page-header-title">
+                                <div class="page-header-icon"><i data-feather="home"></i></div>
+                                <?php echo $entity_name ?>
                             </h1>
+                            <div class="page-header-subtitle">
+                            <?php
+                            $sql_entity = "SELECT * FROM entity WHERE id = '$entity_id' LIMIT 1";
+                            $result = $conn->query($sql_entity);
+
+                            if ($result) {
+                                if (mysqli_num_rows($result) > 0) {
+                                    $entity_data = mysqli_fetch_assoc($result);
+                                    $date_created = $entity_data['date_created'];
+                                } else {
+                                    $date_created = "N/A";
+                                }
+                            }
+
+                            echo "Date Created: $date_created<br>";
+
+                            $sql_user = "SELECT * FROM user WHERE (id = '$entity_id' AND entity_super = 1) LIMIT 1";
+                            $result = $conn->query($sql_user);
+
+
+                            if ($result) {
+                                if (mysqli_num_rows($result) > 0) {
+                                    $user_data = mysqli_fetch_assoc($result);
+                                    echo "Entity Head: $user_data <br>";
+                                }
+                            }
+                            ?>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </header>
         <!-- Main page content-->
-        <div class="container-fluid px-4">
+        <div class="container-fluid pt-5 px-4">
             <div class="card">
-                <div class="class-header" style='padding:25px'>
-                    <?php 
-                        echo "<h1>$entity_name</h1>";
-
-                        $sql_entity = "SELECT * FROM entity WHERE id = '$entity_id' LIMIT 1";
-                        $result = $conn->query($sql_entity);
-
-                        if ($result) {
-                            if (mysqli_num_rows($result) > 0) {
-                                $entity_data = mysqli_fetch_assoc($result);
-                                $date_created = $entity_data['date_created'];
-                            } else {
-                                $date_created = "N/A";
-                            }
-                        }
-                        
-                        echo "<h4>Date Created: $date_created</h4>";
-
-                        $sql_user = "SELECT * FROM user WHERE (id = '$entity_id' AND entity_super = 1) LIMIT 1";
-                        $result = $conn->query($sql_user);
-
-                        if ($result) {
-                            if (mysqli_num_rows($result) > 0) {
-                                $user_data = mysqli_fetch_assoc($result);
-                                echo "<h4>Entity Head: $user_data </h2>";
-                            }
-                        }
-                        
-
-                    ?> 
-                </div>
                 <div class="card-body">
                     <div id="tablePreloader">
                         <p class="text-white p-3">Loading...</p>
                     </div>
                     <table id="datatablesSimple" style="display: none">
                         <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Parent</th>
-                        </tr>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Parent</th>
+                            </tr>
                         </thead>
                         <tfoot>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Parent</th>
-                        </tr>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Parent</th>
+                            </tr>
                         </tfoot>
                         <tbody>
-                        <?php
+                            <?php
 
-                        // select all the departments for the entity
-                        $sql_department = "SELECT * FROM department WHERE id = '$entity_id'";
-                        $result = $conn->query($sql_department);
+                            // select all the departments for the entity
+                            $sql_department = "SELECT * FROM department WHERE id = '$entity_id'";
+                            $result = $conn->query($sql_department);
 
-                        if ($result) {
-                            if (mysqli_num_rows($result) > 0) {
-                                while ($row = $result->fetch_assoc()) {
-                                    $department_id = $row['id'];
-                                    $department_name = $row['name'];
-                                    $department_parent = $row['parent'];
+                            if ($result) {
+                                if (mysqli_num_rows($result) > 0) {
+                                    while ($row = $result->fetch_assoc()) {
+                                        $department_id = $row['id'];
+                                        $department_name = $row['name'];
+                                        $department_parent = $row['parent'];
 
-                                    echo "<tr data-id='$department_id' ><td>$department_id</td><td>$department_name</td><td>$department_parent</td></tr>";
+                                        echo "<tr data-id='$department_id' ><td>$department_id</td><td>$department_name</td><td>$department_parent</td></tr>";
+                                    }
+                                } else {
+                                    header('Location: entity.php');
                                 }
-                            } else {
-                                header('Location: entity.php');
                             }
-                        }
-                        ?>
+                            ?>
                         </tbody>
                     </table>
                 </div>
