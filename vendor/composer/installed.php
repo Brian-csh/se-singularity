b30409e9026784cfa7753715524d4c0b21e89d2f
Ruslan Bayramov <38631709+byrvr@users.noreply.github.com>
2023-04-12 12:17:26 +0800

    fix(pipeline): add autoload files back

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '80f84d424eefba2bdb8db3664607c05868c169b6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '80f84d424eefba2bdb8db3664607c05868c169b6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
