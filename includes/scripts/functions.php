c1ee718be7894b7e752d4bf8564a6c2238803419
孙迪往 <2021080087@secoder.net>
2023-05-14 08:29:32 +0000

    feat: insert log entries indicating Feishu syncs and new Feishu users (#39)

--- includes/scripts/functions.php
+++ includes/scripts/functions.php
@@ -120,6 +120,72 @@ function insert_log_asset_user($conn,$initiator,$participant = null,$asset_id,$r
     }
 }
 
+
+/**
+ * Inserts a log entry indicating that an entity was synced to Feishu by an initiator.
+ * The log type of an entity Feishu sync is 50.
+ *
+ * @param {object} $conn - The database connection object.
+ * @param {number} $entity_id - The ID of the entity that was synced to Feishu.
+ * @param {number} $initiator_id - The ID of the user who initiated the sync.
+ * @return {string} A message indicating whether the record was inserted successfully or an error occurred.
+ *
+ * @example
+ * insert_log_feishu_sync($conn, 123, 456);
+ *
+ * @throws {Error} If the database query fails.
+ */
+function insert_log_feishu_sync($conn, $entity_id, $initiator_id)
+{
+    $time_now = time();
+    $entity_name = mysqli_fetch_array($conn->query("SELECT name FROM entity WHERE id = '$entity_id'"))['name'];
+    $text = "Synced entity " . $entity_name. "to Feishu by " . $initiator_id;
+    $sql = "INSERT INTO log (date, text, log_type, subject,`By`) VALUES
+            ('$time_now','$text','50','$entity_id','$initiator_id')";
+    if ($conn->query($sql)){
+        return "Record inserted successfully.";
+    } else {
+        return "ERROR: Could not able to execute $sql. " . $conn->error;
+    }
+}
+
+
+/**
+ * Inserts a log entry indicating that a new user joined Singularity through Feishu.
+ *
+ * @param {object} $conn - The database connection object.
+ * @param {object} $row - The row from the `user` table containing information about the new user.
+ * @return {string} A message indicating whether the record was inserted successfully or an error occurred.
+ *
+ * @example
+ * $row = array('name' => 'John Doe', 'id' => 123, 'entity' => 456, 'role' => 4);
+ * insert_log_new_feishu_user($conn, $row);
+ *
+ * @throws {Error} If the database query fails.
+ */
+function insert_log_new_feishu_user($conn, $row)
+{
+    /*  BEGIN INSERT LOG */
+    $time_now = time();
+    $username = $row['name'];
+    $user_id = $row['id'];
+    //Fetch Entity, Role, Department
+    $entity_id = $row['entity'];
+    $role_id = $row['role'];
+    $type_id = 51;
+
+    $entity = mysqli_fetch_array($conn->query("SELECT name FROM entity WHERE id = '$entity_id'"))['name'];
+    $role = mysqli_fetch_array($conn->query("SELECT role FROM role WHERE id = '$role_id'"))['role'];
+    $text = $username." joined Singularity through Feishu! ".$role." in ".$entity;
+    $sql = "INSERT INTO log (date, text,log_type, subject) VALUES 
+    ('$time_now','$text','$type_id','$user_id')";
+    if( $conn->query($sql)){
+        return "Record inserted successfully.";
+    } else{
+        return "ERROR: Could not able to execute $sql. " . $conn->error;
+    }
+}
+
 // Request functions
 function make_request($conn,$initiator,$participant = null,$asset_ids,$request_type){
     $time = time();
