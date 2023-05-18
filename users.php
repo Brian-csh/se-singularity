f2d58b0e4b6be442dc59ff8ee1f030e4e93116ae
hws2002 <hanys21@mails.tsinghua.edu.cn>
2023-05-18 17:02:30 +0800

    fix : only admin can add new user

--- users.php
+++ users.php
@@ -54,7 +54,7 @@ switch($role_id){
                                 <div class="page-header-icon text-white"><i data-feather="user"></i></div>
                                 <?=$active?>
                             </h1>
-                            <?php if($role_id <=2){ ?>
+                            <?php if($role_id ==2){ ?>
                             <a href="new_user.php" class="btn btn-primary btn-xs float-end">+ Add</a>
                             <?php }?>
                         </div>
