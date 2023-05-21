0ef3cff751b6a59daae3a084511e034e714593aa
孙迪往 <2021080087@secoder.net>
2023-05-21 21:34:16 +0800

    fix: callback (#71)

--- includes/feishu/third_approval_callback.php
+++ includes/feishu/third_approval_callback.php
@@ -1,52 +1,38 @@
 <?php
-
+$rootPath = $_SERVER['DOCUMENT_ROOT'];
+$includePath = $rootPath . '/includes/db/connect.php';
+include($includePath);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $payload = file_get_contents('php://input');
 
     // Convert the JSON string into a PHP object
-    if($payload == NULL) {
+    if(empty($payload)) {
         http_response_code(400);
         exit();
     }
-    $data = json_decode($payload);
-    // write data in the database
-    $sql = "UPDATE entity SET temp_column = '$payload' WHERE id = 1";
-    $sql = "UPDATE entity SET temp_column = '$data' WHERE id = 2";
-    if (!$conn->query($sql)) {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+    $data = json_decode($payload, true);
+
+    // print the data
+    $actionType = $data["action_type"];
+
     $instanceId = $data['instance_id']; // from pending_requests database, id column
-    $sql = "UPDATE pending_requests SET result = 1 WHERE id = '$instanceId'";
-    if (!$conn->query($sql)) {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+
+    if($actionType == "APPROVE") {
+        // update the pending_requests table
+        // 1 is approved, 2 is rejected
+        // set review_time to current time
+        $sql = "UPDATE pending_requests SET status = 1, review_time = NOW() WHERE id = $instanceId";
+        if (!$conn->query($sql)) {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    } else if($actionType == "REJECT") {
+        // update the pending_requests table
+        $sql = "UPDATE pending_requests SET status = 2, review_time = NOW() WHERE id = $instanceId";
+        if (!$conn->query($sql)) {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     }
-
-    // Access the individual fields from the data array
-    // $actionType = $data['action_type']; // APPROVE or REJECT
-    // $actionContext = $data['action_context']; // 123456
-    // $userId = $data['open_id'];
-    // $approvalCode = $data['approval_code']; // from database, column
-    // $instanceId = $data['instance_id']; // from pending_requests database, id column
-    // $taskId = $data['task_id']; // ignorable
-    // $reason = $data['reason']; // ignorable
-    // $attachments = $data['attachments']; // ignorable, no attachable fields in the form
-    // $token = $data['token']; // sdjkljkx9lsadf110
-
-    // if($actionType == "APPROVE") {
-    //     // update the pending_requests table
-    //     // 1 is approved, 2 is rejected
-    //     $sql = "UPDATE pending_requests SET status = 1 WHERE id = $instanceId";
-    //     if (!$conn->query($sql)) {
-    //         echo "Error: " . $sql . "<br>" . $conn->error;
-    //     }
-    // } else if($actionType == "REJECT") {
-    //     // update the pending_requests table
-    //     $sql = "UPDATE pending_requests SET status = 2 WHERE id = $instanceId";
-    //     if (!$conn->query($sql)) {
-    //         echo "Error: " . $sql . "<br>" . $conn->error;
-    //     }
-    // }
 
     http_response_code(200);
     exit();
