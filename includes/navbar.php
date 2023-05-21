b4b8058ddd3745163fbf588bc1cfb0eb654da324
罗兰 <2021080066@secoder.net>
2023-05-21 18:12:17 +0800

    feat(navbar_third_party_urls): display urls in the navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,6 +4,34 @@ $user_name = $_SESSION['user']['name'];
 $role_id = $_SESSION['user']['role'];
 $entity_id = $_SESSION['user']['entity'] ? $_SESSION['user']['entity'] : -1;
 $department_id = $_SESSION['user']['department'] ? $_SESSION['user']['department'] : -1;
+
+
+// select all the departments for the entity
+$sql_navbar = "SELECT urls FROM entity WHERE id='$entity_id'";
+$result = $conn->query($sql_navbar);
+
+// Declare an empty navbar array
+$navbar_links = array();
+
+$row = $result->fetch_assoc();
+$navbar_links_json = $row['urls'];
+
+if ($navbar_links_json != null) {
+    $navbar_links = json_decode($navbar_links_json);
+}
+
+// Get only the links for the current role
+if (isset($navbar_links->$role_id)) {
+    $navbar_links = $navbar_links->$role_id;
+} else {
+    $navbar_links = array();
+}
+
+// Check if array is empty
+if (empty($navbar_links)) {
+    $navbar_links = array();
+}
+
 ?>
 
 <style>
@@ -141,6 +169,19 @@ $department_id = $_SESSION['user']['department'] ? $_SESSION['user']['department
                             Requests
                         </a>
                     <?php } ?>
+                    <?php if (!empty($navbar_links)) : ?>
+
+                        <!-- Third Party links display-->
+                        <div class="sidenav-menu-heading">Third-Party Links</div>
+
+                        <?php foreach ($navbar_links as $link) : ?>
+                            <a class="nav-link" href="<?= $link->url ?>" target="_blank">
+                                <div class="nav-link-icon"><i data-feather="link"></i></div>
+                                <?= $link->name ?>
+                            </a>
+                        <?php endforeach; ?>
+
+                    <?php endif; ?>
                 </div>
             </div>
             <!-- Sidenav Footer-->
