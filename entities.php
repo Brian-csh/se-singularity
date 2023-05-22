edfd9575bc3d1fa137ab61f46518390f22817cc5
hws2002 <hanys21@mails.tsinghua.edu.cn>
2023-05-22 18:25:16 +0800

    change : add new entity -> modal in entities.php

--- entities.php
+++ entities.php
@@ -4,7 +4,7 @@ include "includes/header.php";
 include "includes/navbar.php";
 // ONLY ACCESSABLE BY SUPER ADMIN
 $sql = "SELECT * FROM entity ORDER BY id DESC";
-
+$popupCondition ="undefined";
 $sync_result = 0;
 // Handle sync success/error messages
 if(isset($_GET['sync_success'])) {
@@ -16,6 +16,20 @@ if(isset($_GET['sync_error'])) {
     $sync_result = 2;
     $error = $_GET['sync_error'];
 }
+
+if (isset($_POST['add_entity'])) {
+    $name = $_POST['entity_name'];
+    $time = time();
+    $sql_add_entity = "INSERT INTO entity (name, date_created) 
+        VALUES ('$name','$time')";
+    // $popupCondition = $conn->query($sql_add_entity) ? "success" : "failure";
+    if($conn->query($sql_add_entity)) {
+        //TODO : popup success / failure
+        $popupCondition = "success";
+    } else {
+        $popupCondition = "failure";
+    }
+}
 ?>
 
 <style>
@@ -26,7 +40,68 @@ if(isset($_GET['sync_error'])) {
     transform: translateX(-50%);
     z-index: 9999;
   }
+  /* POP-up for new entity */
+  .popup {
+    display: none;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+  }
+
+  .popup-content {
+    text-align: center;
+  }
 </style>
+
+<!-- <script>
+  function showSuccessPopup() {
+    document.getElementById("success-popup").style.display = "block";
+  }
+
+  // Show failure popup
+  function showFailurePopup() {
+    document.getElementById("failure-popup").style.display = "block";
+  }
+
+  // Hide all popups
+  function hideAllPopups() {
+    document.getElementById("success-popup").style.display = "none";
+    document.getElementById("failure-popup").style.display = "none";
+  }
+
+  // Call the appropriate function based on the assigned condition after the page loads
+  window.addEventListener("load", function() {
+    var popupCondition = "<?php // echo $popupCondition; ?>";
+    if (popupCondition === "success") {
+      showSuccessPopup();
+    } else if(popupCOnidtion === "failure") {
+      showFailurePopup();
+    } else {
+      hideAllPopups();
+    }
+  });
+</script> -->
+
+<!-- Popup for success -->
+<!-- <div id="success-popup" class="popup">
+  <div class="popup-content">
+    <h3>Success</h3>
+    <p>Your entity has been added successfully.</p>
+  </div>
+</div> -->
+
+<!-- Popup for failure -->
+<!-- <div id="failure-popup" class="popup">
+  <div class="popup-content">
+    <h3>Failure</h3>
+    <p>Sorry, an error occurred while adding the entity.</p>
+  </div>
+</div> -->
 
 <?php if ($sync_result == 1 && $added_amount > 0):?>
     <div class='alert alert-success alert-dismissible fade show' role='alert'>
@@ -56,7 +131,7 @@ if(isset($_GET['sync_error'])) {
                                 <div class="page-header-icon text-white"><i data-feather="home"></i></div>
                                 <?=$active?>
                             </h1>
-                            <a href="new_entity.php" class="btn btn-primary btn-xs float-end">+ Add entity</a>
+                            <button type="button" class="btn btn-primary btn-xs float-end" data-bs-toggle="modal" data-bs-target="#addEntityModal">+ Add Entity</button>
                         </div>
                     </div>
                 </div>
@@ -111,6 +186,31 @@ if(isset($_GET['sync_error'])) {
         </div>
     </main>
 
+    <!-- MODAL -->
+    <!-- ADD entity Modal -->
+    <div class="modal fade" id="addEntityModal" tabindex="-1" role="dialog" aria-labelledby="classAddLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add New Entity</h5>
+                    <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="entities.php" method="post" enctype="multipart/form-data">
+                    <div class="modal-body">
+
+                        <div class="mb-3">
+                            <label for="entityName">Entity Name *</label>
+                            <input class="form-control" id="entityName" type="text" name="entity_name" placeholder="Please input new entity name" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Close</button>
+                        <button class="btn btn-success" type="submit" name="add_entity">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
     <script src="js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="js/jquery-3.6.0.min.js"></script>
     <script src="js/scripts.js"></script>
